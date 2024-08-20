872862f9932073e41fc091102207a781b2e221af
gowgow12 <101544955+gowgow12@users.noreply.github.com>
2024-08-21 07:21:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function plusOneScoreToHome(){
+    let added = 1
+    let homeScore = document.getElementById("home-score")
+    homeScore.textContent = Number(homeScore.textContent)+added
+}
+function plusTwoScoreToHome(){
+    let added = 2
+    let homeScore = document.getElementById("home-score")
+    homeScore.textContent = Number(homeScore.textContent)+added
+}
+function plusThreeScoreToHome(){
+    let added = 3
+    let homeScore = document.getElementById("home-score")
+    homeScore.textContent = Number(homeScore.textContent)+added
+}
+
+function plusOneScoreToGuest(){
+    let added = 1
+    let homeScore = document.getElementById("guest-score")
+    homeScore.textContent = Number(homeScore.textContent)+added
+}
+function plusTwoScoreToGuest(){
+    let added = 2
+    let homeScore = document.getElementById("guest-score")
+    homeScore.textContent = Number(homeScore.textContent)+added
+}
+function plusThreeScoreToGuest(){
+    let added = 3
+    let homeScore = document.getElementById("guest-score")
+    homeScore.textContent = Number(homeScore.textContent)+added
+}
+
+// let gg = Number(document.getElementById("guest-score"))
+// let hh= Number(document.getElementById("home-score"))
+
+// if(Number(document.getElementById("home-score"))>Number(document.getElementById("guest-score"))){
+//     document.getElementById("homeTitle")+=":Winner!"
+// }
+// else{
+//     document.getElementById("guestTitle")+=":Winner!"
+// }
+
